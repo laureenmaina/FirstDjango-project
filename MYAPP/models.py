84da5b94ebd9f5223d15371fbb2536165549a5e1
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2023-04-12 10:18:14 +0300

    Update models.py

--- MYAPP/models.py
+++ MYAPP/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    firstname=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    Age=models.IntegerField()
+    phone_number=models.IntegerField()
+
+    class Meta:
+        db_table: "Student"
+
+    def __str__(self):
+        return self.firstname + ' ' +self.lastname
