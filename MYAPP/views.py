743843f6e9d1ba4ed7c5d4ffa52d21b0ae0fb2fc
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2023-04-12 10:19:06 +0300

    Update views.py

--- MYAPP/views.py
+++ MYAPP/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 def background(request):
     return render(request,"textonimage.html")
+def bills(request):
+    return render(request,"biling.html")
+def image(request):
+    return render(request,"images.html")
+def members(request):
+    all=Student.objects.all().values()
+    details={
+        "members":all
+         }
+    return render(request,"members.html",details)
+def Student(request):
+    stud=StudentForm()
+    return render(request,'student.html',{'form':stud})
+def Employee(request):
+    emp=EmployeeForm()
+    return render(request,'employee.html',{'form':emp})
