82624ac26aadfdfda71f0d5202cb9f65e9d5cc85
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2023-04-14 10:37:34 +0300

    Update admin.py

--- MYAPP/admin.py
+++ MYAPP/admin.py
@@ -3,4 +3,7 @@ from MYAPP.models import Student
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Employees)
+admin.site.register(Venue)
+admin.site.register(Post)
 
