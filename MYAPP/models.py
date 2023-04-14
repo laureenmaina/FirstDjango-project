ae1293743b3b333fa1ea5b5fd1aa3bb2c277f145
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2023-04-14 10:32:42 +0300

    Create models.py

--- MYAPP/models.py
+++ MYAPP/models.py
@@ -13,3 +13,33 @@ class Student(models.Model):
 
     def __str__(self):
         return self.firstname + ' ' +self.lastname
+    
+ class Employees(models.Model):
+    Name=models.CharField(max_length=50)
+    Position=models.CharField(max_length=50)
+    Office=models.CharField(max_length=50)
+    Age=models.IntegerField(null=True)
+    Startdate=models.DateField()
+    Salary=models.IntegerField()
+
+    def  __str__(self):
+        return self.Name+' '+self.Position
+
+class Venue(models.Model):
+    Venuename=models.CharField(max_length=50)
+    Address=models.CharField(max_length=50)
+    ZipPostCode=models.IntegerField()
+    ContactPhone=models.IntegerField()
+    WebAddress=models.IntegerField()
+    EmailAddress=models.EmailField()
+
+    def __str__(self):
+        return self.Venuename+ ' '+self.Address
+
+class Post(models.Model):
+    Author=models.IntegerField(max_length=50)
+    Title=models.CharField(max_length=50)
+    Text=models.TextField()
+    Createddate=models.DateTimeField()
+    Publisheddate=models.DateTimeField()
+
